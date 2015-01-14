3ff2ca6c4d6b20028539dfb8b9c52ff10dff9b67
Ryan Houck <ryan@levelskies.com>
2015-01-14 15:47:10 -0800

    added tests for company and ipo objects

--- trade/tests.py
+++ trade/tests.py
@@ -24,8 +24,6 @@ class EnvVarsTests(TestCase):
 		self.assertTrue(PARSE_CONFIG_DEV['api_key'])
 
 
-from parse_rest.user import User as ParseUser
-
 class ApiConnectionTests(TestCase):
 	"""
 	check connections to external apis and services
@@ -66,8 +64,8 @@ class UserSignupTests(TestCase):
 		self.email = "test@test.com"
 		self.username = "test"
 		self.password = "test_password"
-		self.user_count = sum([i for i in ParseUser.Query.all()])
-		self.ledger_count = sum([i for i in CashLedger.Query.all()])
+		self.user_count = sum([1 for i in ParseUser.Query.all()])
+		self.ledger_count = sum([1 for i in CashLedger.Query.all()])
 
 	def tearDown(self):
 		"""
@@ -79,10 +77,10 @@ class UserSignupTests(TestCase):
 		user.delete()
 
 		# confirm number of user and cash ledger objects has not changed as a result of testing
-		post_user_count = sum([i for i in ParseUser.Query.all()])
+		post_user_count = sum([1 for i in ParseUser.Query.all()])
 		self.assertEqual(self.user_count, post_user_count)
 		
-		post_ledger_count = sum([i for i in CashLedger.Query.all()])
+		post_ledger_count = sum([1 for i in CashLedger.Query.all()])
 		self.assertEqual(self.ledger_count, post_ledger_count)
 
 	
@@ -91,10 +89,9 @@ class UserSignupTests(TestCase):
 		"""
 		test invalid email
 		"""
-		email = 'email'
 		raised = False
 		try:
-			user_signup(email, self.username, self.password)
+			user_signup('email', self.username, self.password)
 		except:
 			raised = True
 		self.assertTrue(raised, 'Did not recognize bad email format')
@@ -102,10 +99,9 @@ class UserSignupTests(TestCase):
 		"""
 		test invalid email value type
 		"""		
-		email = 1
 		raised = False
 		try:
-			user_signup(email, self.username, self.password)
+			user_signup(1, self.username, self.password)
 		except:
 			raised = True
 		self.assertTrue(raised, 'Did not recognize incorrect email value')
@@ -113,10 +109,9 @@ class UserSignupTests(TestCase):
 		"""
 		test invalid username value type
 		"""
-		username = 1
 		raised = False
 		try:
-			user_signup(self.email, username, self.password)
+			user_signup(self.email, 1, self.password)
 		except:
 			raised = True
 		self.assertTrue(raised, 'Did not recognize incorrect username value')
@@ -124,10 +119,9 @@ class UserSignupTests(TestCase):
 		"""
 		test invalid password value type
 		"""
-		password = 1
 		raised = False
 		try:
-			user_signup(self.email, self.username, password)
+			user_signup(self.email, self.username, 1)
 		except:
 			raised = True
 		self.assertTrue(raised, 'Did not recognize incorrect password value')
@@ -156,5 +150,163 @@ class UserSignupTests(TestCase):
 			raised = True
 		self.assertTrue(raised, 'Created duplicate user')
 
+class CompanyTests(TestCase):
+	
+	def setUp(self):
+		
+		register(PARSE_CONFIG_DEV['app_id'], PARSE_CONFIG_DEV['api_key'])
+
+		self.name = "TestCo"
+		self.ticker = "TST"
+
+		self.co_count = sum([1 for i in Company.Query.all()])
+		
+	def tearDown(self):
+		company = Company.Query.get(ticker=self.ticker)
+		company.delete()
+
+		# confirm number of company objects has not changed as a result of testing
+		post_co_count = sum([1 for i in Company.Query.all()])
+		self.assertEqual(self.co_count, post_co_count)
+
+	def test_create_company_and_duplicates(self):
+		
+		"""
+		test impropper inputs
+		"""
+		raised = False
+		try:
+			create_company(1, self.ticker)
+		except:
+			raised = True
+		self.assertTrue(raised, 'Did not catch impropper name value')
+
+		raised = False
+		try:
+			create_company(self.name, 1)
+		except:
+			raised = True
+		self.assertTrue(raised, 'Did not catch impropper ticker value')
+
+	
+		"""
+		test propper company creation
+		"""
+		raised = False
+		try:
+			create_company(self.name, self.ticker)
+		except:
+			raised = True
+		self.assertFalse(raised, 'Could not create test company')
+
+		"""
+		test creating duplicate companies
+		"""
+		raised = False
+		try:
+			create_company(self.name, 'tickertest')
+		except:
+			raised = True
+		self.assertTrue(raised, 'Could create duplicate name company')
+
+		raised = False
+		try:
+			create_company('nametest', self.ticker)
+		except:
+			raised = True
+		self.assertTrue(raised, 'Could create duplicate ticker company')
 
 
+class IPOTests(TestCase):
+	
+	def setUp(self):
+		
+		register(PARSE_CONFIG_DEV['app_id'], PARSE_CONFIG_DEV['api_key'])
+
+		self.name = "TestCo"
+		self.ticker = "TST"
+		self.price = 10.0
+		self.auth = 1000000
+		
+		self.ipo_count = sum([1 for i in IPO.Query.all()])
+
+		# create test company
+		create_company(self.name, self.ticker)
+	
+	def tearDown(self):
+		
+		company = Company.Query.get(ticker=self.ticker)
+		company.delete()
+
+		try:
+			# certain tests don't result in created IPO item so can't force delete object
+			ipo_item = IPO.Query.get(ticker=self.ticker)
+			ipo_item.delete()
+		except:
+			pass
+
+		# confirm number of ipo objects has not changed as a result of testing
+		post_ipo_count = sum([1 for i in IPO.Query.all()])
+		self.assertEqual(self.ipo_count, post_ipo_count)
+
+	# test ipo where company doesn't exist
+	# test input values to create IPO object - try inappropriate vals
+	# try creating duplicate IPOs
+
+	def test_create_ipo_object_where_co_not_exist(self):
+		raised = False
+		try:
+			create_ipo_object('BADTICKER', self.price, self.auth)
+		except:
+			raised = True
+		self.assertTrue(raised, "Created IPO object not tied to existing company")
+	
+	def test_create_ipo_object(self):
+
+		"""
+		test impropper inputs
+		"""
+		raised = False
+		try:
+			create_ipo_object(1, self.price, self.auth)
+		except:
+			raised = True
+		self.assertTrue(raised, 'Did not catch impropper ticker value')
+
+		raised = False
+		try:
+			create_ipo_object(self.ticker, 'price', self.auth)
+		except:
+			raised = True
+		self.assertTrue(raised, 'Did not catch impropper price value')
+
+		raised = False
+		try:
+			create_ipo_object(self.ticker, self.price, 'auth')
+		except:
+			raised = True
+		self.assertTrue(raised, 'Did not catch impropper auth value')
+
+	
+		"""
+		test propper ipo creation
+		"""
+		raised = False
+		try:
+			create_ipo_object(self.ticker, self.price, self.auth)
+		except Exception as err:
+			print err
+			raised = True
+		self.assertFalse(raised, 'Could not create test IPO')
+
+	def test_creation_of_duplicate_ipo_objects(self):
+		"""
+		test creating duplicate ipo objects
+		"""
+		raised = False
+		try:
+			create_ipo_object(self.ticker, self.price, self.auth)
+			create_ipo_object(self.ticker, self.price, self.auth)
+		except:
+			raised = True
+		self.assertTrue(raised, 'Could create duplicate name company')
--- trade/utils.py
+++ trade/utils.py
@@ -10,7 +10,8 @@ class Company(Object):
     pass
 class CashLedger(Object):
     pass
-
+class IPO(Object):
+    pass
 
 def gen_alphanum_key():
     key = ''
@@ -18,17 +19,7 @@ def gen_alphanum_key():
         key += choice(string.uppercase + string.lowercase + string.digits)
     return key
 
-def get_company_by_ticker(ticker):
-	"""
-	finds company in parse db matching ticker
-	"""
-	if not isinstance(ticker, str):
-		raise ValueError('ticker must be string.')
-	company = Company.Query.get(ticker=ticker)
-	return company
-
-
-def offer(user_id, dir, quant, px=None):
+def offer(user_id, dir, quant, price=None):
 	"""
 	use this function to offer to buy or sell shares
 	user_id - parse user id
@@ -37,20 +28,6 @@ def offer(user_id, dir, quant, px=None):
 	px - None if market, float if limit
 	"""
 	pass
-
-def create_parse_user(email, username, password):
-	"""
-	creates new user object in parse db
-	"""
-
-	if not isinstance(username, str) or not isinstance(email, str) or not isinstance(password, str):
-		raise ValueError('username, email, and password must be strings.')
-
-	email = email.lower()
-	ref = gen_alphanum_key()
-	
-	user = ParseUser.signup(username=username, password=password, email=email, ref=ref)
-	return user	
 
 def update_cash_ledger(user_id, amount):
 	"""
@@ -67,6 +44,21 @@ def update_cash_ledger(user_id, amount):
 	cash_ledger_item = CashLedger(user_id=user_id, amount=amount)
 	cash_ledger_item.save()
 	return cash_ledger_item
+
+
+def create_parse_user(email, username, password):
+	"""
+	creates new user object in parse db
+	"""
+
+	if not isinstance(username, str) or not isinstance(email, str) or not isinstance(password, str):
+		raise ValueError('username, email, and password must be strings.')
+
+	email = email.lower()
+	ref = gen_alphanum_key()
+	
+	user = ParseUser.signup(username=username, password=password, email=email, ref=ref)
+	return user	
 
 def user_signup(email, username, password):
 	"""
@@ -89,6 +81,73 @@ def user_signup(email, username, password):
 		raise Exception(err)
 
 	return user
+
+def create_company(name, ticker):
+	"""
+	create parse company object
+	"""
+	if not isinstance(name, str):
+		raise ValueError('name must be string.')
+	if not isinstance(ticker, str):
+		raise ValueError('ticker must be string.')
+	
+	#check for duplicate companies
+	companies = Company.Query.all().filter(ticker=ticker)
+	count = sum([1 for i in companies])
+	if count:
+		raise Exception('Company with this ticker already in db.')
+
+	companies = Company.Query.all().filter(name=name)
+	count = sum([1 for i in companies])
+	if count:
+		raise Exception('Company with this name already in db.')
+	
+	company = Company(name=name, ticker=ticker)
+	company.save()
+	return company
+
+def get_company_by_ticker(ticker):
+	"""
+	finds company in parse db matching ticker
+	"""
+	if not isinstance(ticker, str):
+		raise ValueError('ticker must be string.')
+	company = Company.Query.get(ticker=ticker)
+	return company
+
+def create_ipo_object(ticker, price, auth):
+	"""
+	creates an parse db object to represent the shares made available in a public offering for a company
+	
+	ticker - string representing company ticker
+	px - float representing price per shares
+	auth - int representing number of shares available for purchase
+	
+	sold - int representing estimated number of shares sold from ipo
+	"""
+	if not isinstance(ticker, str):
+		raise ValueError('ticker must be string.')
+	if not isinstance(price, float):
+		raise ValueError('price must be float.')
+	if not isinstance(auth, int):
+		raise ValueError('auth must be int.')
+	if price <= 0:
+		raise ValueError('price must be positive.')
+	if auth <= 0:
+		raise ValueError('auth must be positive.')
+	
+	company = get_company_by_ticker(ticker)
+
+	#check for duplicate companies
+	ipo_objects = IPO.Query.all().filter(company_id=company.objectId)
+	count = sum([1 for i in ipo_objects])
+	if count:
+		raise Exception('IPO object for this company in db.')
+
+	ipo_object = IPO(company_id=company.objectId, ticker=ticker, price=price, auth=auth)
+	ipo_object.save()
+	return ipo_object
+
 
 if __name__ == "__main__":
 	
